a77d7090faaf05ce783387f4e8e047870ecfd309
sanatiel <sanatielbarros90@gmail.com>
2022-06-03 16:52:00 -0300

    at

--- src/ui/components/List/style.tsx
+++ src/ui/components/List/style.tsx
@@ -1,11 +1,14 @@
 import { styled } from '@mui/material';
 
 export const ListStyled = styled('ul')`
-
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
+    padding: ${props => props.theme.spacing(2)};    
 `;
 
 export const ListItem = styled('li')`
-    
+    display: grid;
 `;
 
 export const Picture = styled('img')`
